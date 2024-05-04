e0222a882eb2945704f2fbfbb9129e60d70ce290
Adam Ostoja-Helczynski <ahelczynski@gmail.com>
2024-05-04 12:25:32 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,11 +1,17 @@
 import company.beans.Person;
 import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
+import company.interfaces.Messenger;
 
 public class Main {
     public static void main(String[] args) {
         try {
             Person person = new Person("Adam Ostoja-Hełczyński", 30);
             System.out.println("Osoba: " + person.getName() + ", Wiek: " + person.getAge());
+
+            Messenger emailMessenger = new EmailMessenger();
+            emailMessenger.sendMessage("Witaj, " + person.getName() + "!");
+
         } catch (InvalidAgeException e) {
             System.err.println("Wystąpił błąd przy tworzeniu osoby: " + e.getMessage());
         }
